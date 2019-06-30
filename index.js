0eebb99bfbf5c43b7c22498aceba349e2c43d849
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-29 23:19:02 -0300

    66. N07 - Separando Rotas do Arquivo Principal

--- index.js
+++ index.js
@@ -1,32 +1,11 @@
 const express = require('express');
+let routesIndex = require('./routes/index');
+let routesUsers = require('./routes/users');
 
 const app = express();
 
-
-
-app.get('/', (req, res) => {
-
-
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	res.end('<h1>Olá</h1>');
-});
-
-
-app.get('/users', (req, res) => {
-
-	res.statusCode = 200;
-	res.setHeader('Content-Type', 'text/html');
-	res.json({
-		users: [{
-			name: 'Hcode',
-			email: 'contato@hcode.com.br',
-			id: 1
-		}]
-	});
-
-});
-
+app.use(routesIndex);
+app.use('/users',routesUsers);
 
 app.listen(3000, ()=> {
 
---
+++ routes/users.js
@@ -0,0 +1,26 @@
+let express = require('express');
+let routes = express.Router();
+
+routes.get('/', (req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.json({
+        users: [{
+            name: 'Hcode',
+            email: 'contato@hcode.com.br',
+            id: 1
+        }]
+    });
+});
+
+routes.get('/admin', (req, res) => {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.json({
+        users: []
+    });
+});
+
+module.exports = routes;
