f0106aafc27f65641b4c991df010bf853157117e
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-30 19:37:52 -0300

    74. N15 - Excluindo um usuário

--- routes/users.js
+++ routes/users.js
@@ -52,13 +52,25 @@ module.exports = (app) => {
     routeId.put((req, res) => {
 
         db.update({_id: req.params.id}, req.body, err => {
-                    if (err) {
+                    if (err){
                         app.utils.error.send(err, req, res);
                     } else {
                         res.status(200).json(Object.assign(req.params, req.body));
                     }
                 }
             );
+    });
+
+    routeId.delete((req, res) => {
+
+        db.remove({_id: req.params.id}, {}, err => {
+                if (err){
+                    app.utils.error.send(err, req, res);
+                } else {
+                    res.status(200).json(req.params);
+                }
+            }
+        );
     });
 
 };
