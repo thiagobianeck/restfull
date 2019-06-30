275b43d8771c391d861cb7738f7fcd0aaf3655ad
Thiago Bianeck <thiagobianeck@gmail.com>
2019-06-30 18:51:42 -0300

    70. N11 - Listando os usuários do banco NeDB

--- routes/users.js
+++ routes/users.js
@@ -8,15 +8,21 @@ module.exports = (app) => {
 
     app.get('/users', (req, res) => {
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.json({
-            users: [{
-                name: 'Hcode',
-                email: 'contato@hcode.com.br',
-                id: 1
-            }]
-        });
+        db.find({})
+            .sort({name: 1})
+            .exec((err, users) => {
+                if(err) {
+                    console.log(`error: ${err}`);
+                    res.status(400).json({
+                       error: err
+                    });
+
+                } else {
+                    res.status(200).json(users);
+                }
+            }
+        );
+
     });
 
     app.post('/users', (req, res) => {
